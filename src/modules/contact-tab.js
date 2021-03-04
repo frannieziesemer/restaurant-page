2a83a706e8e00ed29c23114217d88db8f1aba01b
frannie <frances.ziesemer@gmail.com>
2021-03-04 14:57:01 +0100

    finish loading data to menu and contact tabs

--- src/modules/contact-tab.js
+++ src/modules/contact-tab.js
@@ -1,9 +1,28 @@
-const contactContent = 'CONTACT Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh cras pulvinar mattis nunc sed blandit. Lacus viverra vitae congue eu consequat ac felis donec. Aliquet nec ullamcorper sit amet risus. At volutpat diam ut venenatis tellus in. Quis varius quam quisque id diam vel quam. Aliquam vestibulum morbi blandit cursus. Porta nibh venenatis cras sed. Quam vulputate dignissim suspendisse in. Ut eu sem integer vitae justo eget magna fermentum iaculis. Tristique senectus et netus et malesuada fames. Tristique senectus et netus et. Metus dictum at tempor commodo ullamcorper. Fusce id velit ut tortor pretium viverra suspendisse potenti nullam. Accumsan tortor posuere ac ut consequat semper viverra. Elementum nibh tellus molestie nunc non blandit massa enim nec. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum. Diam maecenas sed enim ut sem viverra. Amet tellus cras adipiscing enim eu. Enim tortor at auctor urna. At varius vel pharetra vel turpis nunc eget lorem. Diam ut venenatis tellus in metus vulputate eu scelerisque felis. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin.'
+//using object to construct contact details
+const contactDetails = {
+  email: 'email@email.com',
+  phone: '01 2345 6789',
+  address: '1 Street St, City 1234'
+}
 
-const createContent = (text) => {
-  const content = document.createElement('p');
+//create the html elements and insert data from object 
+const createContent = (object) => {
+  const content = document.createElement('div');
   content.classList.add('contact-content');
-  content.textContent = text;
+  for(let property in object) {
+    const contactWrapper = document.createElement('div');
+    contactWrapper.classList.add('contact-wrapper');
+    
+    const contactProperty = document.createElement('p');
+    contactProperty.textContent = property;
+
+    const contactValue = document.createElement('p');
+    contactValue.textContent = object[property];
+
+    contactWrapper.appendChild(contactProperty);
+    contactWrapper.appendChild(contactValue);
+    content.appendChild(contactWrapper);
+  }
   return content;
 }
 
@@ -11,7 +30,7 @@ const createContent = (text) => {
 const loadContactTab = () => {
   const tabContent = document.getElementById('main');
   tabContent.innerHTML = '';
-  tabContent.appendChild(createContent(contactContent));
+  tabContent.appendChild(createContent(contactDetails));
 
 
 }
